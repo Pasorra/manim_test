e3538cadbe8c5e16b25121107689ea4c11142a5b
Pasorra <56963478+Pasorra@users.noreply.github.com>
2024-01-04 00:35:05 +0300

    progress on multiplication

--- scene.py
+++ scene.py
@@ -1,5 +1,9 @@
 from manim import *
 
+SHORT = 0.25
+MEDIUM = 0.5
+LONG = 2
+
 
 def colorIJK(ijk: MathTex, ijk_tex_strings: [str], ijk_colors: [color]) -> None:
     for v in zip(ijk_tex_strings, ijk_colors):
@@ -27,19 +31,19 @@ class VektorelCarpim(Scene):
 
         for eq in range(1, len(g), 2):
             colorIJK(g[eq], g[eq].tex_strings[1::2], ijk_colors)
-
+        UL
         self.play(
             LaggedStart(
                 Create(g[:2]),
                 Create(g[2:]),
                 lag_ratio=0.4,
             ),
-            run_time=3,
+            run_time=MEDIUM,
         )
 
         # removing A = and B =
         self.play(ShrinkToCenter(vecA), ShrinkToCenter(vecB))
-        self.play(g.remove(vecA, vecB).animate.center(), run_time=2)
+        self.play(g.remove(vecA, vecB).animate.center())
 
         # Separating out IJK
         ijk = MathTex(
@@ -54,7 +58,6 @@ class VektorelCarpim(Scene):
             TransformMatchingShapes(eqA.copy(), ijk),
             TransformMatchingShapes(eqA, newEqA),
             TransformMatchingShapes(eqB, newEqB),
-            run_time=2,
         )
 
         # Create matrix
@@ -65,7 +68,7 @@ class VektorelCarpim(Scene):
         )
         for i in range(3):
             m0[0][i].color = ijk_colors[i]
-        self.play(TransformMatchingShapes(newG, m0), run_time=3)
+        self.play(TransformMatchingShapes(newG, m0), run_time=2)
 
         # Write out AxB next to matrix
         axb = MathTex(r"A{\times}B=")
@@ -73,30 +76,59 @@ class VektorelCarpim(Scene):
         multEq = VGroup(axb, m1).arrange()
         self.play(TransformMatchingShapes(m0, m1))
         self.wait()
-        self.play(Write(axb), run_time=2)
-        self.play(multEq.animate.to_corner(LEFT, buff=0.5), run_time=2)
+        self.play(Write(axb))
 
         # Write signs above matrix columns
-        text_objects = []
         for col_index in range(len(m1.get_columns()[0])):
             text = MathTex("+" if col_index % 2 == 0 else "-").set_color(
                 GREEN if col_index % 2 == 0 else RED
             )
-            text_objects.append((text, col_index))
-        for text, col_index in text_objects:
             text.add_updater(
                 lambda x, col_index=col_index: x.next_to(
                     [m1.get_columns()[col_index].get_x(), m1.get_rows()[0].get_y(), 0],
                     UP * (1.4 if col_index % 2 == 0 else 2),
                 )
             )
-            self.play(Write(text))
+            self.play(Write(text), run_time=SHORT)
 
-        # Select and highlight rows-cols
+        entries_to_dim = VGroup()
+        entries_to_highlight = VGroup()
+
+        # Dim rows-cols
         for col in range(len(m1.get_mob_matrix())):
             for row in range(len(m1.get_mob_matrix()[col])):
+                entry = m1.get_mob_matrix()[col][row]
+                if col == 0 and row == 0:
+                    continue
                 if col == 0 or row == 0:
-                    entry = m1.get_mob_matrix()[col][row]
-                    self.play(entry.animate.set_opacity(0.2), run_time=0.1)
+                    entries_to_dim.add(entry)
+                else:
+                    entries_to_highlight.add(entry)
+
+        self.play(
+            LaggedStart(
+                *[ApplyMethod(entry.set_opacity, 0.5) for entry in entries_to_dim],
+                lag_ratio=0.2,
+                run_time=0.5
+            )
+        )
+
+        # highlight bottom right of the matrix
+        self.play(multEq.animate.to_corner(LEFT, buff=0.5))
+        self.play(Circumscribe(entries_to_highlight), run_time=1.5)
+        eq_sign = MathTex("=").next_to(m1)
+        self.play(Write(eq_sign), run_time=SHORT)
+
+        start_x, start_y = (1, 1)
+        i = m1.get_mob_matrix()[0][0].copy()
+        up_left = m1.get_mob_matrix()[start_y][start_x].copy()
+        up_right = m1.get_mob_matrix()[start_y][start_x + 1].copy()
+        down_left = m1.get_mob_matrix()[start_y + 1][start_x].copy()
+        down_right = m1.get_mob_matrix()[start_y + 1][start_x + 1].copy()
+        i_mult = m1.get_mob_matrix()[0][0].copy()
+        g_i_mult = VGroup(i_mult).next_to(eq_sign).arrange()
+        self.play(TransformMatchingShapes(i, g_i_mult), run_time=SHORT)
+        g_i_mult += MathTex("(5 * 2)")
+        g_i_mult.arrange()
 
         self.wait(2)
